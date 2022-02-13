93d8d9a3250670652719d72f0d4269b8fbeed37e
olgaSimankova <o.kotlyarova@gmail.com>
2022-02-13 12:02:52 +0300

    feat: add stack algorythm but it's still not working properly with ||

--- src/index.js
+++ src/index.js
@@ -1,3 +1,40 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+
+  const bracketsPair = bracketsConfig.reduce(function (previousValue, currentItem) {
+    previousValue[currentItem[1]] = currentItem[0];
+    return previousValue;
+  }, {})
+
+  const openBrackets = bracketsConfig.reduce(function (previousValue, currentItem) {
+    previousValue.push(currentItem[0]);
+    return previousValue;
+  }, [])
+
+  let stack = []
+
+  // const verticalBrQty = str.match(/[|]/g).length ? str.match(/[|]/g).length : 0;
+  // let verticalBrCount = 0
+
+  for (let i = 0; i < str.length; i++) {
+
+    let current = str[i]
+
+    if (openBrackets.includes(current)) {
+      stack.push(current)
+    } else {
+
+      if (stack.length === 0) {
+        return false
+      }
+
+      let topItem = stack[stack.length - 1]
+
+      if (bracketsPair[current] === topItem) {
+        stack.pop()
+      } else {
+        return false
+      }
+    }
+  }
+  return stack.length == 0
 }
